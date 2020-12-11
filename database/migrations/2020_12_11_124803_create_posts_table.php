11479e72b88b7cd9aa8372d657ac690ad3437627
ashrafuddin1496 <asraf1496@gmail.com>
2020-12-11 19:30:51 +0600

    database created and migrated successful

--- database/migrations/2020_12_11_124803_create_posts_table.php
+++ database/migrations/2020_12_11_124803_create_posts_table.php
@@ -17,7 +17,6 @@ class CreatePostsTable extends Migration
             $table->id();
             $table->string('title');
             $table->string('slug');
-            $table->string('slug');
             $table->unsignedInteger('category_id');
             $table->string('content');
             $table->timestamps();
