ae4f933bb82a459c3fb75e11eadcb591a576bb68
ashrafuddin1496 <asraf1496@gmail.com>
2020-12-15 09:56:52 +0600

    use foreign key at hasone method only

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,7 +11,7 @@ class Post extends Model
     protected $guarded = [];
 
     public  function category(){
-        return $this ->belongsTo(Category::class,'category_id','id');
+        return $this ->belongsTo(Category::class);
     }
 
 
